486bd29019b8d96c77fc5642e62ac5b399c906fe
Norah Almaneea <nouraalmanea0@gmail.com>
2023-04-18 09:13:09 +0300

    components

---
+++ src/Components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+   <>
+    <footer>
+      DDDesigned and developed by Courses4Arab.com
+      <span>🧡</span>
+    </footer>
+   </>
+  )
+}
